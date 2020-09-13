4a4d414b342e6c85f9503b19d670b563f22634c9
Govindarajan Narasimhan <govindarajan_narasimhan@apple.com>
2020-09-13 06:55:42 +0530

    Initial commit.

---
+++ src/test/java/com/govind/springit/SpringitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.govind.springit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
